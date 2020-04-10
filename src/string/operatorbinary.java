17b9f1af724852dead77ad77bb46946ff023290f
mahdi <kyosjelo@gmail.com>
2020-04-10 10:37:00 +0700

    OPERATOR BINARY ASSIGNMENT

--- src/string/operatorbinary.java
+++ src/string/operatorbinary.java
@@ -8,4 +8,13 @@ public class operatorbinary {
     String nama = "Muhammad Mahdi";
     int jumlahSks = 24;
     double indekxPrestasiKumulatif = 3.89;
+
+    public static void  mainn (String[] args ){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
