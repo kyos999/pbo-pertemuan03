acdd32e02f0e1bebc2d67ebb0ef0f8b0d0662089
mahdi <kyosjelo@gmail.com>
2020-04-10 10:42:51 +0700

    OPERATOR BINARY ASSIGNMENT

--- src/operator/operatorbinary.java
+++ src/operator/operatorbinary.java
@@ -1,10 +1,6 @@
-package string;
+package operator;
 
 public class operatorbinary {
-
-    //operator (=) sebagai assignment/penugasan
-    //menempatkan nilai kedalam variable
-    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
     String nama = "Muhammad Mahdi";
     int jumlahSks = 24;
     double indekxPrestasiKumulatif = 3.89;
